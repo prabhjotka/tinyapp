390f10ab5fe2ad58586cb2a4893364347b5cf336
Prabh <sonukaur68@gmail.com>
2023-10-01 09:54:20 -0600

    adding post request

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+function generateRandomString() { }
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,10 +7,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -17,6 +21,10 @@ app.get("/urls/:id", (req, res) => {
       urlDatabase[req.params.id]
   };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 // app.get("/hello", (re, res) => {
 //   res.send("<html<body><h1>Welcome to my web page </h1></body></html> ");
