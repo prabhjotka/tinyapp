ee91baa4a1ee5ce1e438fb8c0ea7d830a7d15603
Prabh <sonukaur68@gmail.com>
2023-10-01 07:38:24 -0600

    chenges to express server file bu introducing html template

--- express_server.js
+++ express_server.js
@@ -6,12 +6,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+app.get("/hello", (re, res) => {
+  res.send("<html<body><h1>Welcome to my web page </h1></body></html> ");
 });
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
