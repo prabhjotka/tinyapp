c6018f4971382b9a476b0d376b5d00fa34c0d777
Prabh <sonukaur68@gmail.com>
2023-10-01 08:42:35 -0600

    Adding new route

--- express_server.js
+++ express_server.js
@@ -6,16 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id, longURL:
+      urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
 });
 // app.get("/hello", (re, res) => {
 //   res.send("<html<body><h1>Welcome to my web page </h1></body></html> ");
