f34ec8182dc6715f871c7b6d6f1b4924969c38a3
Prabh <sonukaur68@gmail.com>
2023-10-03 22:27:27 -0600

    setting cookies

--- express_server.js
+++ express_server.js
@@ -10,7 +10,9 @@ function generateRandomString(length) {
   return result;
 }
 const express = require("express");
+var cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const urlDatabase = {
@@ -19,18 +21,26 @@ const urlDatabase = {
 };
 app.use(express.urlencoded({ extended: true }));
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 //creating a newUrl page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+
+  };
+  res.render("urls_new", templateVars);
 });
 //showing a short url version for longUrl
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id, longURL:
-      urlDatabase[req.params.id]
+      urlDatabase[req.params.id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -70,13 +80,18 @@ app.get("/urls", (req, res) => {
 //submit new url
 app.post("/urls/:id", (req, res) => {
 
-  console.log(req.body);
+  //console.log(req.body);  for debugguing purpose
   urlDatabase[req.params.id] = req.body.newUrl;
 
   res.redirect("/urls/");
 
 });
+//Handle post to login to set cookie
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
 
+  res.redirect("/urls");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
